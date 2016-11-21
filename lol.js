2cced5a9bf8478826b5ddb3c8597b3c032b27528
Goncalo <goncaloflf@hotmail.com>
2016-11-21 19:14:43 +0000

    started circles

--- lol.js
+++ lol.js
@@ -1,4 +1,11 @@
-var dataset;
+/*var dataset;
+var currentChampion;
+var diameter = 200;
+
+var bubble = d3.layout.pack()
+		.sort(null)
+		.size([diameter,diameter])
+		.padding(1.5);
 
 d3.json("champions.json", function(data) {
 	dataset = data;
@@ -7,7 +14,14 @@ d3.json("champions.json", function(data) {
 })
 
 function genFirst(){
-	var svg = d3.select("#champSelLeft");
+	var w = 361.6;
+	var h = 312.5;
 
-	
-}
+	var svg = d3.select("#champSelLeft")
+				.append("svg")
+				.attr("width",w)
+				.attr("height", h);
+
+
+
+}*/
