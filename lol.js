2aff911ad7804b86f581e56dbd253c233252a15f
Goncalo <goncaloflf@hotmail.com>
2016-11-22 14:52:38 +0000

    added stuff

--- lol.js
+++ lol.js
@@ -1,30 +1,3 @@
-/*var dataset;
-var currentChampion;
-var diameter = 200;
-
-var bubble = d3.layout.pack()
-		.sort(null)
-		.size([diameter,diameter])
-		.padding(1.5);
-
-d3.json("champions.json", function(data) {
-	dataset = data;
-
-	genFirst();
-})
-
-function genFirst(){
-	var w = 361.6;
-	var h = 312.5;
-
-	var svg = d3.select("#champSelLeft")
-				.append("svg")
-				.attr("width",w)
-				.attr("height", h);
-
-
-
-}*/
 
 function highlight(stuff) {
 	var child = document.getElementById("champSelLeft").children;
@@ -33,4 +6,12 @@ function highlight(stuff) {
 	}
 
 	document.getElementById(stuff.id).className=("highlight");
+}
+
+function clickChamps() {
+	var child = document.getElementById("champSelLeft").children;
+	for(i = 0; i < child.length ; i++){
+		document.getElementById(child[i].id).className = "";
+		document.getElementById(child[i].id).style.visibility = "visible";
+	}
 }
