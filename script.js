eef5cc0ea7b9210217e298407475381b2d3d5c97
Gonçalo Ferreira <goncaloflf@hotmail.com>
2016-11-25 17:32:48 +0000

    panda says: fml

---
+++ script.js
@@ -0,0 +1,82 @@
+var w = 180,
+    h = 180;
+
+var colorscale = d3.scaleOrdinal(d3.schemeCategory10);
+
+//Legend titles
+var LegendOptions = ['Smartphone','Tablet'];
+
+//Data
+var d = [
+          [
+            {axis:"Damage Dealt",value:0.59},
+            {axis:"Damage Taken",value:0.56},
+            {axis:"Minions per Game",value:0.42},
+            {axis:"Gold Earned",value:0.34},
+            {axis:"Kills",value:0.48},
+            {axis:"Deaths",value:0.08}
+          ]
+        ];
+
+//Options for the Radar chart, other than default
+var mycfg = {
+  w: w,
+  h: h,
+  maxValue: 0.6,
+  levels: 6,
+  ExtraWidthX: 300
+}
+
+//Call function to draw the Radar chart
+//Will expect that data is in %'s
+RadarChart.draw("#starplot", d, mycfg);
+
+////////////////////////////////////////////
+/////////// Initiate legend ////////////////
+////////////////////////////////////////////
+
+var svg = d3.select('#body')
+    .selectAll('svg')
+    .append('svg')
+    .attr("width", w+300)
+    .attr("height", h)
+
+//Create the title for the legend
+var text = svg.append("text")
+    .attr("class", "title")
+    .attr('transform', 'translate(90,0)') 
+    .attr("x", w - 70)
+    .attr("y", 10)
+    .attr("font-size", "12px")
+    .attr("fill", "#404040")
+    .text("What % of owners use a specific service in a week");
+
+//Initiate Legend   
+var legend = svg.append("g")
+    .attr("class", "legend")
+    .attr("height", 100)
+    .attr("width", 200)
+    .attr('transform', 'translate(90,20)') 
+    ;
+    //Create colour squares
+    legend.selectAll('rect')
+      .data(LegendOptions)
+      .enter()
+      .append("rect")
+      .attr("x", w - 65)
+      .attr("y", function(d, i){ return i * 20;})
+      .attr("width", 10)
+      .attr("height", 10)
+      .style("fill", function(d, i){ return colorscale(i);})
+      ;
+    //Create text next to squares
+    legend.selectAll('text')
+      .data(LegendOptions)
+      .enter()
+      .append("text")
+      .attr("x", w - 52)
+      .attr("y", function(d, i){ return i * 20 + 9;})
+      .attr("font-size", "11px")
+      .attr("fill", "#737373")
+      .text(function(d) { return d; })
+      ; 
